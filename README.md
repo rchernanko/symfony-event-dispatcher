ebb403026c288c096b636c83e451e6f443d1e56b
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-09-08 09:07:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#Practice - Symfony Event Dispatcher
+
+- My notes based on the symfony component event dispatcher tutorial on the laracasts website
+- https://laracasts.com/series/discover-symfony-components/episodes/4
+
+##Set up
+
+All I did to create this project (and this can be applied to any symfony projects):
+
+- mkdir <project_name>
+- cd <project_name>
+- composer require symfony/event-dispatcher
+
+Of course, this assumes I already have composer installed on my device
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+//Always need to do this somewhere for all my classes to automatically load!
+require 'vendor/autoload.php';
+
+//Creates an event dispatcher "container" but with no listeners bound to it yet...
+$dispatcher = new EventDispatcher;
+
+//Now let's add a listener...
+
+//Think of the listener as 'someone listening for somebody else to broadcast something that took place'
+$dispatcher->addListener('user.becamePremium');
